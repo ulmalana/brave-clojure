9b384401034e904ab85af6c014315fda402ada7f
rz <muhamaul@gmail.com>
2022-07-12 14:20:29 +0700

    [Clojure] finished chapter 1 and 2

---
+++ ch01-get-started/clojure01/src/clojure01/core.clj
@@ -0,0 +1,11 @@
+(ns clojure01.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Guten Tag!"))
+
+(defn test-cider
+  []
+  (println "this is a test using cider in spacemacs"))
